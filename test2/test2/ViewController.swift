6a759dbf54a44e0aa94fd3b74b0d33698c59d02b
Jihyun <jkim68888@gmail.com>
2024-07-26 16:15:46 +0900

    test2 생성

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 김지현 on 7/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
